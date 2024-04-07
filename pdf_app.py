fef6963af99bdd41e5c3d0675a0357be120fd350
Debaditya Bhar <debaditya.bhar@outlook.com>
2024-04-07 12:46:37 +0530

    Version 1.1.2 - Completed all the functionalities.

--- pdf_app.py
+++ pdf_app.py
@@ -1,26 +1,21 @@
+# PDF Manager Application
+
 # This is PDF manager application. It has following functionalities:
-# 1. Merge PDF documents into a single document when user provides the input path of the PDFs.
-# 2. User input the path of a PDF file and list of page numbers of the file. The application create a new
-# PDF document using the page numbers.
-# 3. User input the path of a PDF file. The application extract all the text from the file and write them
-# into a text file.
+# 1. User input the path of a PDF file. The application extracts all the text from the file 
+#    and write them into a text file.
+# 2. User input the path of a PDF file and list of page numbers of the file. 
+#    The application creates a new PDF document using the page numbers.
+# 3. Merge PDF documents into a single document when user provides the input path of the PDFs.
 
 # This application is desktop application using text base input/output.
 
 from pdf_manage import PDFManager
+import os
 
-def convert_page_number(in_list):
-    """This function converts the range into page numbers."""
-
-    out_list = []
-    for item in in_list:
-        if type(item) is list:
-            for num in range(item[0], item[1] + 1, 1):
-                out_list.append(num)
-        else:
-            out_list.append(item)
-    
-    return out_list
+# All the functions.
+def clear_screen():
+    """This function clears the console."""
+    os.system('clear' if os.name == 'posix' else 'cls')
 
 
 def get_user_choice():
@@ -30,19 +25,73 @@ def get_user_choice():
     print('\n\t1. Extract Text from a PDF File.')
     print('\t2. Crop a PDF File to create new PDF file.')
     print('\t3. Merge multiple of PDF files into a single PDF file.')
+    print('\t4. Exit application.')
     choice = int(input('\n\tEnter Your Choice: '))
+    return choice
 
 
-choice = get_user_choice()
+def extract():
+    print('\n\tExtract Text from PDF file')
+    print('\t--------------------------\n')
+    input_pdf_file = input('\tFull path of your PDF file: ').replace('\\', '/')
+    output_text_file = input('\tFull path of the output text file: ').replace('\\', '/')
 
-# page_num = convert_page_number([1,3,5, [10, 17], 23])
-# print(page_num)
+    pdf = PDFManager(input_pdf_file)
+    pdf.extract_pdf_text(output_text_file)
 
-# pdf : PDFManager = PDFManager(r'D:/MyLearning/AWS/AWS Notes/PDF Notes/Developer Associate Services')
 
-# pdf.extract_pdf_text(r'D:/Personal/My Resume/resume.txt')
+def crop():
+    print('\n\tCrop PDF file')
+    print('\t-------------\n')
+    input_pdf_file = input('\tFull path of your PDF file: ').replace('\\', '/')
+    page_list_prompt = """\n\tNow you will enter list of pages to crop from your PDF file. The page number
+    is you enter in whole number. When you finish entering page numbers, just enter any keyboard character
+    to stop input.\n"""
+    print(page_list_prompt)
+    page_list = []
 
-# pdf.crop_pdf_file([0,1,4], r'D:/Personal/My Resume/346863_short_resume.pdf')
+    will_input = True
+    while will_input: 
+        try: 
+            page = int(input('\tEnter page number - '))
+            page_list.append(page)
+        except ValueError:
+            will_input = False
+    
+    output_pdf_file = input('\tFull path of Output PDF file: ').replace('\\', '/')
 
-# pdf.merge_pdf(r'D:/MyLearning/AWS/AWS Notes/PDF Notes/all.pdf')
+    pdf = PDFManager(input_pdf_file)
+    pdf.crop_pdf_file(page_list, output_pdf_file)
+
+
+def merge():
+    print('\n\tMerge multiple PDF files')
+    print('\t------------------------\n')
+    input_pdf_path = input('\tFull path where your PDF files reside: ').replace('\\', '/')
+    output_pdf_file = input('\tFull path of the Output PDF file (After Merge): ').replace('\\', '/')
+
+    pdf = PDFManager(input_pdf_path)
+    pdf.merge_pdf(output_pdf_file)
+
+
+def stop():
+    print('\n\tGood Bye!\n')
+    global app_active 
+    app_active = False
+    clear_screen()
+
+# Main Program flow.
+app_active = True
+while app_active:
+    choice = get_user_choice()
+
+    match choice:
+        case 1:
+            extract()
+        case 2:
+            crop()
+        case 3:
+            merge()
+        case 4:
+            stop()
 
